a65ffa07b1777d047f0a0a90049b9a190223d2ca
jdues01 <98050045+jdues01@users.noreply.github.com>
2022-05-13 12:55:38 -0400

    Add files via upload

---
+++ DuesJacksonFinalProject.py
@@ -0,0 +1,9 @@
+# Program Name: Inches to Meters Conversion
+# Author: Jackson Dues
+# Date: 4/21/2022
+# Summary: convert a measurement in inches entered by user into meters
+# Variables:
+#   inches: measurement in inches entered by the user (int)
+#   meters: measurement in meters (float)
+
+import InchestoMetersConversion
