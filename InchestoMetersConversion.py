7ba259b445f84c52d3c244420ba7c880299d5661
jdues01 <98050045+jdues01@users.noreply.github.com>
2022-05-13 13:10:53 -0400

    Add files via upload

---
+++ InchestoMetersConversion.py
@@ -0,0 +1,17 @@
+# Program Name: Inches to Meters Conversion
+# Author: Jackson Dues
+# Date: 5/12/2022
+# Summary: convert a measurement in inches entered by user into meters
+# Variables:
+#   inches: measurement in inches entered by the user (float)
+#   meters: measurement in meters (float)
+
+def main():
+    # Get measurement in inches from user
+    inches = float(input("How many inches do you want to convert? "))
+    while inches <= 0:
+        inches = float(input("ERROR...Please enter a number greater than 0: "))
+    # Compute meters and display result with label
+    meters = inches * 0.0254
+    print("Measurement in meters: ", format(meters, '.4f'))
+main()
